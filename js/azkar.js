1f308fbc31c8db1cc70aa1bc4d6e0cf0119fef7d
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-01-10 17:17:29 +0200

    re-design

--- js/azkar.js
+++ js/azkar.js
@@ -15,7 +15,7 @@ function fetchAzkarData() {
     return;
   }
 
-  fetch("../data/azkar.json")
+  fetch("./data/azkar.json")
     .then((response) => {
       if (!response.ok) throw new Error("Network response was not ok");
       return response.json();
